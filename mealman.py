96ed1f2b5326938944db844064ca63b517826df9
Ziad Arafat <ziadarafat1@gmail.com>
2022-10-19 20:57:23 -0600

    Changed output to integer for readability

--- mealman.py
+++ mealman.py
@@ -48,6 +48,6 @@ if __name__ == "__main__":
             else:
                 if "#" not in row[0]:
                     calories_in_plan += int(row[1]) * calorie_dict[row[0]]
-    print("Calories Allowed Today:", max_calories)
-    print("Calories In Plan:", calories_in_plan)
-    print("Excess calories: ", calories_in_plan - max_calories)
+    print("Calories Allowed Today:", int(max_calories))
+    print("Calories In Plan:", int(calories_in_plan))
+    print("Excess calories: ", int(calories_in_plan - max_calories))
